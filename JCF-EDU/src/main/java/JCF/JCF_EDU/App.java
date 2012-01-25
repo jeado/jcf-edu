42326f129d720148f986dbd394992490eb05df1e
Jeado <haibane84@gmail.com>
2012-01-25 11:48:50 +0900

    최초공유

---
+++ JCF-EDU/src/main/java/JCF/JCF_EDU/App.java
@@ -0,0 +1,13 @@
+package JCF.JCF_EDU;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
